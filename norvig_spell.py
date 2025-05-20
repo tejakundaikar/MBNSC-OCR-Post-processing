d122014a4f5b71b2ff261f088fcb84af57d2f057
tejakundaikar <tkundaikar10@gmail.com>
2025-05-20 16:42:55 +0530

    Update norvig_spell.py

--- norvig_spell.py
+++ norvig_spell.py
@@ -1,7 +1,4 @@
 """Spelling Corrector in Python 3; see http://norvig.com/spell-correct.html
-
-Copyright (c) 2007-2016 Peter Norvig
-MIT license: www.opensource.org/licenses/mit-license.php
 """
 
 import re
